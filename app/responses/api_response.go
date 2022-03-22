a90b15fbd91ad9643dd10bd6a65e5b3d3c635311
I Kadek Adi Gunawan <adi_gunawan@live.com>
2022-03-22 22:33:51 +0800

    feat(users): :sparkles: echo web server
    application

---
+++ app/responses/api_response.go
@@ -0,0 +1,85 @@
+package responses
+
+import (
+	"github.com/ALTA-BE7-I-Kadek-Adi-Gunawan/clibank/app/users"
+	"github.com/ALTA-BE7-I-Kadek-Adi-Gunawan/clibank/app/wallets"
+)
+
+type ApiResponse struct {
+	Message    string      `json:"message"`
+	StatusCode int         `json:"status_code"`
+	Error      string      `json:"error"`
+	Data       interface{} `json:"data"`
+}
+
+type UsersApiResponse struct {
+	Message    string         `json:"message"`
+	StatusCode int            `json:"status_code"`
+	Data       []UserResponse `json:"data"`
+}
+
+type UserApiResponse struct {
+	Message    string       `json:"message"`
+	StatusCode int          `json:"status_code"`
+	Data       UserResponse `json:"data"`
+}
+
+type WalletResponse struct {
+	// ID        uint    `json:"id"`
+	Balance   float64 `json:"balance"`
+	Status    string  `json:"status"`
+	Currency  string  `json:"currency"`
+	CreatedAt string  `json:"created_at"`
+	UpdatedAt string  `json:"updated_at"`
+}
+
+func (wallet *WalletResponse) FromModel(model *wallets.Wallet) {
+	// wallet.ID = model.ID
+	wallet.Balance = model.Balance
+	wallet.Status = model.Status
+	wallet.Currency = model.Currency
+	wallet.CreatedAt = model.CreatedAt.String()
+	wallet.UpdatedAt = model.UpdatedAt.String()
+}
+
+type AccountResponse struct {
+	// ID          uint           `json:"id"`
+	Name string `json:"name"`
+	// PhoneNumber string         `json:"phone_number"`
+	Wallet    WalletResponse `json:"wallet"`
+	CreatedAt string         `json:"created_at"`
+	UpdatedAt string         `json:"updated_at"`
+}
+
+func (account *AccountResponse) FromModel(userModel users.Account) {
+	// account.ID = userModel.ID
+	account.Name = userModel.Name
+	// account.PhoneNumber = userModel.PhoneNumber
+	account.CreatedAt = userModel.CreatedAt.String()
+	account.UpdatedAt = userModel.UpdatedAt.String()
+	if userModel.Wallet != nil {
+		account.Wallet = WalletResponse{}
+		account.Wallet.FromModel(userModel.Wallet)
+	}
+}
+
+type UserResponse struct {
+	ID          uint            `json:"id"`
+	Email       string          `json:"email"`
+	PhoneNumber string          `json:"phone_number"`
+	Account     AccountResponse `json:"account,omitempty"`
+	CreatedAt   string          `json:"created_at"`
+	UpdatedAt   string          `json:"updated_at"`
+}
+
+func (user *UserResponse) FromModel(userModel users.User) {
+	user.ID = userModel.ID
+	user.Email = userModel.Email
+	user.PhoneNumber = userModel.PhoneNumber
+	user.CreatedAt = userModel.CreatedAt.String()
+	user.UpdatedAt = userModel.UpdatedAt.String()
+	if userModel.Account != nil {
+		user.Account = AccountResponse{}
+		user.Account.FromModel(*userModel.Account)
+	}
+}
---
+++ controller/user_controller.go
@@ -0,0 +1,219 @@
+package controller
+
+import (
+	"strconv"
+
+	"github.com/ALTA-BE7-I-Kadek-Adi-Gunawan/clibank/app/responses"
+	_users "github.com/ALTA-BE7-I-Kadek-Adi-Gunawan/clibank/app/users"
+	"github.com/labstack/echo/v4"
+	"github.com/samber/lo"
+)
+
+type UserController struct {
+	service *_users.UserService
+}
+
+func (ctrlUser *UserController) Init(service *_users.UserService) {
+	ctrlUser.service = service
+}
+
+// GetUsers
+// @Summary Get all users
+// @Description show all users with wallet
+// @Tags users
+// @ID get-users
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} responses.UsersApiResponse	"ok"
+// @Failure 500 {object} responses.ApiResponse "Server Error"
+// @Router /users [get]
+func (ctrl *UserController) GetUsers(c echo.Context) error {
+	users, err := ctrl.service.GetUsers()
+
+	if err != nil {
+		return c.JSON(500, responses.ApiResponse{
+			Message: "failed to get users",
+			Data:    []_users.User{},
+			Error:   err.Error(),
+		})
+	}
+
+	usersResponse := lo.Map(users, func(t _users.User, i int) responses.UserResponse {
+		data := responses.UserResponse{}
+		data.FromModel(t)
+		return data
+	})
+
+	return c.JSON(200, responses.UsersApiResponse{
+		Message: "success",
+		Data:    usersResponse,
+	})
+}
+
+// GetUsers
+// @Summary get user by it id
+// @Description get string by ID
+// @Tags users
+// @ID get-user
+// @Accept  json
+// @Produce  json
+// @Param id path int true "ID"
+// @Success 200 {object} responses.UserApiResponse
+// @Failure 402 {object} responses.ApiResponse "Server Error"
+// @Failure 404 {object} responses.ApiResponse "Server Error"
+// @Router /users/{id} [get]
+func (ctrl *UserController) GetUser(c echo.Context) error {
+	id := c.Param("id")
+	userId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(402, responses.ApiResponse{
+			Message: "invalid id",
+			Data:    nil,
+		})
+	}
+	user, err := ctrl.service.GetById(userId)
+
+	if err != nil {
+		return c.JSON(404, responses.ApiResponse{
+			Message: "user not found",
+			Data:    nil,
+		})
+	}
+	data := &responses.UserResponse{}
+	data.FromModel(*user)
+	return c.JSON(200, responses.UserApiResponse{
+		Message: "success",
+		Data:    *data,
+	})
+}
+
+// UpdateUser
+// @Summary Update user By ID
+// @Description Update string by ID
+// @ID update-user
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param id path string true "ID"
+// @Param data body users.UpdateUserDto true "data"
+// @Success 200 {object} responses.UserApiResponse
+// @Failure 402 {object} responses.ApiResponse "Server Error"
+// @Failure 404 {object} responses.ApiResponse "Server Error"
+// @Router /users/{id} [put]
+func (ctrl *UserController) UpdateUser(c echo.Context) error {
+	id := c.Param("id")
+	userId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(402, responses.ApiResponse{
+			Message: "invalid id",
+			Data:    nil,
+		})
+	}
+
+	userData := new(_users.UpdateUserDto)
+	if err := c.Bind(userData); err != nil {
+		return c.JSON(400, responses.ApiResponse{
+			Message: "invalid body",
+			Data:    nil,
+		})
+	}
+
+	user, err := ctrl.service.GetById(userId)
+
+	if err != nil {
+		return c.JSON(404, responses.ApiResponse{
+			Message: "user not found",
+		})
+	}
+	updatedUser, err := ctrl.service.UpdateUser(user.PhoneNumber, user.Pin, *userData)
+
+	if err != nil {
+		return c.JSON(500, responses.ApiResponse{
+			Message: "failed to update user",
+		})
+	}
+	data := &responses.UserResponse{}
+	data.FromModel(updatedUser)
+	return c.JSON(200, responses.UserApiResponse{
+		Message: "success",
+		Data:    *data,
+	})
+}
+
+// CreateUser
+// @Summary Add a new user
+// @Description add a new user
+// @Tags users
+// @ID create-users
+// @Accept  json
+// @Produce  json
+// @Param data body _users.CreateUserDto true "data"
+// @Success 200 {object} responses.UserApiResponse
+// @Failure 400 {object} responses.ApiResponse "Not valid data"
+// @Failure 500 {object} responses.ApiResponse "Server Error"
+// @Router /users [post]
+func (ctrl *UserController) CreateUser(c echo.Context) error {
+	userData := new(_users.CreateUserDto)
+	if err := c.Bind(userData); err != nil {
+		return c.JSON(400, responses.ApiResponse{
+			Message: "invalid body",
+			Data:    nil,
+		})
+	}
+
+	user, err := ctrl.service.CreateUser(*userData)
+
+	if err != nil {
+		return c.JSON(500, responses.ApiResponse{
+			Message: "failed to create user",
+		})
+	}
+	data := &responses.UserResponse{}
+	data.FromModel(user)
+	return c.JSON(200, responses.UserApiResponse{
+		Message: "success",
+		Data:    *data,
+	})
+}
+
+// DeleteUser
+// @Summary delete user by id
+// @Description delete string by ID
+// @Tags users
+// @ID delete-users
+// @Accept  json
+// @Produce  json
+// @Param id path int true "ID"
+// @Success 200 {object} responses.UsersApiResponse	"ok"
+// @Failure 500 {object} responses.ApiResponse "Server Error"
+// @Router /users/{id} [delete]
+func (ctrl *UserController) DeleteUser(c echo.Context) error {
+	id := c.Param("id")
+	userId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(402, responses.ApiResponse{
+			Message: "invalid id",
+			Data:    nil,
+		})
+	}
+
+	user, err := ctrl.service.GetById(userId)
+
+	if err != nil {
+		return c.JSON(404, responses.ApiResponse{
+			Message: "user not found",
+		})
+	}
+
+	err = ctrl.service.DeleteUser(user.PhoneNumber)
+
+	if err != nil {
+		return c.JSON(500, responses.ApiResponse{
+			Message: "failed to delete user",
+		})
+	}
+	return c.JSON(204, responses.ApiResponse{
+		Message: "success",
+		Data:    nil,
+	})
+}
